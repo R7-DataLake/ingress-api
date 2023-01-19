b0f23923a0f0ffb0e34c8bf701fa98adb0493419
Satit Rianpit <siteslave@users.noreply.github.com>
2023-01-19 16:44:12 +0700

    เพิ่ม Appoint

---
+++ src/routes/appoint.ts
@@ -0,0 +1,37 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+
+import {
+  StatusCodes,
+  getReasonPhrase,
+} from 'http-status-codes';
+
+// โหลด Schema
+import appointSchema from '../schema/appoint';
+
+export default async (fastify: FastifyInstance) => {
+
+  // รับข้อมูล APPOINT
+  fastify.post('/appoint', {
+    onRequest: [fastify.authenticate],
+    schema: appointSchema
+  }, async (request: FastifyRequest, reply: FastifyReply) => {
+
+    try {
+      // Get json from body
+      const data: any = request.body;
+      // Add queue
+      await fastify.bullmq.add("APPOINT", data);
+      // Reply
+      reply
+        .status(StatusCodes.OK)
+        .send(getReasonPhrase(StatusCodes.OK))
+    } catch (error) {
+      request.log.error(error);
+      reply
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR) })
+    }
+
+  })
+
+} 
