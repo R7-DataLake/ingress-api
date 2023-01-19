97b142ea519fa819331617128a0ca65c4d2d006f
Satit Rianpit <siteslave@users.noreply.github.com>
2023-01-19 10:34:20 +0700

    test build failed

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,7 +6,7 @@ import {
 } from 'http-status-codes';
 
 // โหลด Schema
-import personSchema from '../schema/person';
+import personSchemax from '../schema/person';
 import opdSchema from '../schema/opd';
 import chronicSchema from '../schema/chronic';
 import opdxSchema from '../schema/opdx';
