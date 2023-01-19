afd2b6de8d473928305b2e0c01e5d4178dfaf0ce
Satit Rianpit <siteslave@users.noreply.github.com>
2023-01-19 21:10:51 +0700

    add drugallergy

---
+++ src/routes/drugallergy.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+
+import {
+  StatusCodes,
+  getReasonPhrase,
+} from 'http-status-codes';
+
+// โหลด Schema
+import drugallergySchema from '../schema/drugallergy';
+
+export default async (fastify: FastifyInstance) => {
+
+  // รับข้อมูล DRUGALLERGY
+  fastify.post('/drugallergy', {
+    // Verify JWT
+    onRequest: [fastify.authenticate],
+    // Validate schema
+    schema: drugallergySchema
+  }, async (request: FastifyRequest, reply: FastifyReply) => {
+
+    try {
+      // Get json from body
+      const data: any = request.body;
+      // Add queue
+      await fastify.bullmq.add("DRUGALLERGY", data);
+      // Reply
+      reply
+        .status(StatusCodes.OK)
+        .send(getReasonPhrase(StatusCodes.OK))
+    } catch (error) {
+      request.log.error(error);
+      reply
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR) })
+    }
+
+  })
+
+} 
