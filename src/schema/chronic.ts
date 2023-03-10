074fa0269686a86d1ff7344f43f769eccdb9760b
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-10 18:56:58 +0700

    update

--- src/schema/chronic.ts
+++ src/schema/chronic.ts
@@ -6,7 +6,7 @@ const schema = S.array().minItems(1).maxItems(100).items(
     .prop('HN', S.string().maxLength(50).required())
     .prop('DATE_DIAG', S.string().maxLength(8).minLength(8).required())
     .prop('CHRONIC', S.string().maxLength(8).minLength(3).required())
-    .prop('HOSP_DX', S.string().minLength(5).maxLength(10).required())
+    .prop('HOSP_DX', S.string().maxLength(10))
     .prop('HOSP_RX', S.string().maxLength(10))
     .prop('D_UPDATE', S.string().maxLength(14).minLength(14).required())
 )
