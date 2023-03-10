209ea881f0df110ebf6717923ac80ad05d9be9e6
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-10 10:35:40 +0700

    Change `drug` to `opddrug` and `ipddrug`

---
+++ src/routes/ipddrug.ts
@@ -0,0 +1,106 @@
+import { AxiosResponse } from "axios"
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
+
+import {
+  StatusCodes,
+  getReasonPhrase,
+} from 'http-status-codes'
+import _ from "lodash"
+import { DateTime } from "luxon"
+
+const { v4: uuidv4 } = require('uuid')
+
+// โหลด Schema
+import schema from '../schema/ipddrug'
+import convertCamelCase from '../utils'
+
+export default async (fastify: FastifyInstance) => {
+
+  fastify.route({
+    method: 'POST',
+    url: '/ipddrug',
+    schema: schema,
+    handler: async (request: FastifyRequest, reply: FastifyReply) => {
+
+      try {
+        const accessToken: any = request.accessToken;
+        // Verify token
+        const urlIntrospect = process.env.R7PLATFORM_INGR_INTROSPECT_ENDPOINT;
+        const response: AxiosResponse = await fastify.axios.post(urlIntrospect, {}, {
+          headers: {
+            'Authorization': 'Bearer ' + accessToken
+          }
+        });
+
+        const { ingress_zone, hospcode, sub } = response.data;
+
+        // Get json from body
+        const body: any = request.body;
+        const data = convertCamelCase.camelizeKeys(body)
+
+        let isError = false
+
+        data.forEach((i: any) => {
+          if (i.hospcode !== hospcode) {
+            isError = true
+            return
+          }
+        })
+
+        if (isError) {
+          return reply
+            .status(StatusCodes.BAD_REQUEST)
+            .send({
+              error: getReasonPhrase(StatusCodes.BAD_REQUEST),
+              message: 'ไม่ใช่ข้อมูลของหน่วยงาน',
+              statusCode: StatusCodes.BAD_REQUEST
+            })
+        }
+
+        const ingressQueue = fastify.createIngressQueue(ingress_zone)
+        const logQueue = fastify.createLogQueue()
+
+        const now = DateTime.now().toSQL({ includeOffset: false })
+        const trx_id = uuidv4()
+        // Add queue
+        const ingressData: any = {
+          file_name: 'IPDDRUG',
+          trx_id, data, hospcode,
+          ingress_zone, user_id: sub,
+          created_at: now
+        }
+
+        const logData: any = {
+          trx_id, hospcode, ingress_zone,
+          user_id: sub, created_at: now,
+          total_records: _.size(data),
+          file_name: 'IPDDRUG',
+          status: 'sending'
+        }
+
+        await ingressQueue.add("IPDDRUG", ingressData)
+        await logQueue.add('INGRESS', logData)
+
+        reply
+          .status(StatusCodes.OK)
+          .send({ status: 'success', trx_id })
+      } catch (error: any) {
+        request.log.error(error);
+        let message: any;
+        if (_.has(error, 'message')) {
+          message = error.message;
+        } else {
+          message = getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+        reply
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .send({
+            status: 'error',
+            error: message,
+            statusCode: StatusCodes.INTERNAL_SERVER_ERROR
+          });
+      }
+    }
+  })
+
+} 
---
+++ src/schema/opddrug.ts
@@ -0,0 +1,25 @@
+import S from 'fluent-json-schema'
+
+const schema = S.array().minItems(1).maxItems(100).items(
+  S.object()
+    .prop('HOSPCODE', S.string().minLength(5).maxLength(10).required())
+    .prop('HN', S.string().maxLength(50).required())
+    .prop('SEQ', S.string().maxLength(15).required())
+    .prop('DID', S.string().maxLength(30).required())
+    .prop('AMOUNT', S.number().maximum(999999).default(0).required())
+    .prop('DRUGPRICE', S.number().maximum(999999).default(0).required())
+    .prop('DRUGCOST', S.number().maximum(999999).default(0).required())
+    .prop('UNIT', S.string().maxLength(50).required())
+    .prop('UNIT_PACK', S.string().maxLength(20))
+    .prop('SIGCODE', S.string().maxLength(50).required())
+    .prop('PROVIDER', S.string().maxLength(6))
+    .prop('D_UPDATE', S.string().maxLength(14).minLength(14).required())
+)
+
+const headerSchema = S.object()
+  .prop('Authorization', S.string().required())
+
+export default {
+  body: schema,
+  headers: headerSchema
+}
